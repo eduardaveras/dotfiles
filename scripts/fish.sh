9a7186e07e3adc091dc62879110f6f79e7661286
dude <mevm@cin.ufpe.br>
2024-09-23 21:52:50 -0300

    fix install error

--- scripts/fish.sh
+++ scripts/fish.sh
@@ -2,7 +2,7 @@
 sudo apt-get install fish
 
 # Set fish as default shell
-echo (which fish) | sudo tee -a /etc/shells
+echo $(which fish) | sudo tee -a /etc/shells
 
 # Change default shell
-chsh -s (which fish)
+chsh -s $(which fish)
